986b47de29762ef213774791e050fa0393e6929b
w99427 <w99427@gmail.com>
2024-11-28 15:26:11 +0800

    fix

--- BlindAuctionOpt.sol
+++ BlindAuctionOpt.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.4;
-contract BlindAuction {
+contract BlindAuctionOpt {
     struct Bid {
         bytes32 blindedBid;
         uint deposit;
