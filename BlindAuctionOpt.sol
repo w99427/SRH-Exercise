32c2b3c6532e0b833f188d004cdd052de7e1bd73
w99427 <w99427@gmail.com>
2024-11-28 15:04:00 +0800

    Update Opt Version

--- BlindAuctionOpt.sol
+++ BlindAuctionOpt.sol
@@ -7,9 +7,12 @@ contract BlindAuction {
     }
 
     address payable public beneficiary;
-    uint public biddingEnd;
-    uint public revealEnd;
+    address public owner;
+ //   uint public biddingEnd;
+ //   uint public revealEnd;
     bool public ended;
+    bool public biddingEnded;
+    bool public revealEnded;
 
     mapping(address => Bid[]) public bids;
 
@@ -25,34 +28,52 @@ contract BlindAuction {
 
     /// The function has been called too early.
     /// Try again at `time`.
-    error TooEarly(uint time);
+    //error TooEarly(uint time);
     /// The function has been called too late.
     /// It cannot be called after `time`.
-    error TooLate(uint time);
+    //error TooLate(uint time);
+
+    error TimingNotAllowed(bool con1, bool con2, bool con3);
     /// The function auctionEnd has already been called.
     error AuctionEndAlreadyCalled();
+    error PermissionCheckFailed();
 
     // Modifiers are a convenient way to validate inputs to
     // functions. `onlyBefore` is applied to `bid` below:
     // The new function body is the modifier's body where
     // `_` is replaced by the old function body.
-    modifier onlyBefore(uint time) {
-        if (block.timestamp >= time) revert TooLate(time);
+    modifier onlyBefore(bool con) {
+        if (con) revert TimingNotAllowed(biddingEnded, revealEnded, ended);
         _;
     }
-    modifier onlyAfter(uint time) {
-        if (block.timestamp <= time) revert TooEarly(time);
+    modifier onlyAfter(bool con) {
+        if (!con) revert TimingNotAllowed(biddingEnded, revealEnded, ended);
         _;
     }
 
     constructor(
-        uint biddingTime,
-        uint revealTime,
         address payable beneficiaryAddress
     ) {
         beneficiary = beneficiaryAddress;
-        biddingEnd = block.timestamp + biddingTime;
-        revealEnd = biddingEnd + revealTime;
+        owner = msg.sender;
+    }
+    ///set Bidding End Status
+    function setBiddingEnd()
+    external 
+    {
+        if (msg.sender != owner){
+            revert PermissionCheckFailed();
+        }
+        biddingEnded = true;
+    }
+
+    function setRevealEnd()
+    external 
+    {
+        if (msg.sender != owner){
+            revert PermissionCheckFailed();
+        }
+        revealEnded = true;
     }
 
     /// Place a blinded bid with `blindedBid` =
@@ -67,7 +88,7 @@ contract BlindAuction {
     function bid(bytes32 blindedBid)
         external
         payable
-        onlyBefore(biddingEnd)
+        onlyBefore(biddingEnded)
     {
         bids[msg.sender].push(Bid({
             blindedBid: blindedBid,
@@ -84,8 +105,8 @@ contract BlindAuction {
         bytes32[] calldata secrets
     )
         external
-        onlyAfter(biddingEnd)
-        onlyBefore(revealEnd)
+        onlyAfter(biddingEnded)
+        onlyBefore(revealEnded)
     {
         uint length = bids[msg.sender].length;
         require(values.length == length);
@@ -136,9 +157,22 @@ contract BlindAuction {
         }
     }
     ///withdraw only one bid
-    function withdrawaBid(bytes32 blindedBid) external {
-
+    function withdrawOneBid(bytes32 blindedBid) 
+    external
+    onlyBefore(biddingEnded) 
+    {
+        uint refund;
+        //FIXME: What happens if there are multiple identical blindBid in Bids[]?
+        for (uint i=0; i < bids[msg.sender].length; i++){
+            if (bids[msg.sender][i].blindedBid == blindedBid)
+            {
+                refund += bids[msg.sender][i].deposit;
+                payable(msg.sender).transfer(refund);
+                delete bids[msg.sender][i];
+            }
+        }
     }
+
     /// Withdraw a bid that was overbid.
     function withdraw() external {
         uint amount = pendingReturns[msg.sender];
@@ -157,7 +191,7 @@ contract BlindAuction {
     /// to the beneficiary.
     function auctionEnd()
         external
-        onlyAfter(revealEnd)
+        onlyAfter(revealEnded)
     {
         if (ended) revert AuctionEndAlreadyCalled();
         emit AuctionEnded(highestBidder, highestBid);
